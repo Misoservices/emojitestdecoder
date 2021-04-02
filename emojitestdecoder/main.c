6f759ac17127e94079086cd092fe21524b862516
Michel Donais <michel@misoservices.com>
2021-04-02 15:05:40 -0400

    Initial Commit

---
+++ emojitestdecoder/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  emojitestdecoder
+//
+//  Created by Michel Donais on 2021-04-02.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
